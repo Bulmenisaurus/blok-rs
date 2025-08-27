0f3cd45a13ead384ba4c34412f97345c063f4048
Bulmenisaurus <63682890+Bulmenisaurus@users.noreply.github.com>
2025-08-27 00:14:18 -0700

    update

--- src/bin/datagen.rs
+++ src/bin/datagen.rs
@@ -9,26 +9,57 @@ use rand::seq::IndexedRandom;
 use rayon::prelude::*;
 use std::{
     fs::File,
-    io::Write,
-    sync::{Arc, Mutex},
+    io::{BufWriter, Write},
+    sync::atomic::{AtomicU64, Ordering},
     time::Instant,
 };
 
 fn main() {
     let start = Instant::now();
-    let file = Arc::new(Mutex::new(File::create("data.bin").unwrap()));
+    let mut file = BufWriter::new(File::create("data.bin").unwrap());
+    let total_written = AtomicU64::new(0);
 
-    let total: u32 = (0..1).into_par_iter().map(|_| playout(&file)).sum();
+    // Process in batches to allow periodic writes
+    let batch_size = 100; // Adjust based on your needs
+    let total_games = 30usize;
+
+    for batch_start in (0..total_games).step_by(batch_size) {
+        let batch_end = (batch_start + batch_size).min(total_games);
+
+        // Collect batch results
+        let results: Vec<Vec<[u32; 15]>> = (batch_start..batch_end)
+            .into_par_iter()
+            .map(|i| playout(i))
+            .collect();
+
+        // Write batch results
+        for packed_positions in results {
+            let bytes = packed_positions
+                .iter()
+                .flat_map(|p| serialize(*p))
+                .collect::<Vec<u8>>();
+
+            file.write_all(&bytes).unwrap();
+            total_written.fetch_add(packed_positions.len() as u64, Ordering::Relaxed);
+        }
+
+        // Flush to disk periodically
+        file.flush().unwrap();
+        println!(
+            "Written {} positions so far",
+            total_written.load(Ordering::Relaxed)
+        );
+    }
 
     let duration = start.elapsed();
-    println!("Total positions: {}", total);
+    println!("Total positions: {}", total_written.load(Ordering::Relaxed));
     println!(
         "Positions per second: {}",
-        total as f64 / duration.as_secs_f64()
+        total_written.load(Ordering::Relaxed) as f64 / duration.as_secs_f64()
     );
 }
 
-fn playout(file: &Arc<Mutex<File>>) -> u32 {
+fn playout(idx: usize) -> Vec<[u32; 15]> {
     let mut i = 0;
     let mut board = BoardState::new(StartPosition::Corner);
     let mut mcts = MonteCarlo::new();
@@ -37,16 +68,16 @@ fn playout(file: &Arc<Mutex<File>>) -> u32 {
     let mut packed_positions: Vec<[u32; 15]> = Vec::new();
 
     // opening: skip opening moves
-    // for _ in 0..6 {
-    //     let moves = generate_moves(&board);
-    //     let random_move = moves.choose(&mut rng).unwrap();
-    //     board.do_move(*random_move);
-    // }
-
-    let bad_opening = vec![32768, 67152, 34953, 69168, 6162, 70730];
-    for m in bad_opening {
-        board.do_move(m);
+    for _ in 0..6 {
+        let moves = generate_moves(&board);
+        let random_move = moves.choose(&mut rng).unwrap();
+        board.do_move(*random_move);
     }
+
+    // let bad_opening = vec![32768, 67152, 34953, 69168, 6162, 70730];
+    // for m in bad_opening {
+    //     board.do_move(m);
+    // }
 
     while board.game_result() == GameResult::InProgress {
         mcts.run_search(&board, "eval");
@@ -72,74 +103,40 @@ fn playout(file: &Arc<Mutex<File>>) -> u32 {
     }
 
     let result = board.game_result();
-    println!("Result: {:?}", result);
+    let result_str = match board.game_result() {
+        GameResult::PlayerAWon => "A",
+        GameResult::PlayerBWon => "B",
+        GameResult::Draw => "D",
+        GameResult::InProgress => unreachable!(),
+    };
 
-    // annotate all of the packed positions with the result (make sure it's the side to move)
-    for (i, packed) in packed_positions.iter_mut().enumerate() {
-        let side_to_move = i % 2;
+    // annotate all of the packed positions with the result (absolute result)
+    for packed in packed_positions.iter_mut() {
+        // 0 -> A, 1 -> B, 2 -> T
         let result_bits = match result {
-            GameResult::PlayerAWon => {
-                if side_to_move == 0 {
-                    0
-                } else {
-                    1
-                }
-            }
-            GameResult::PlayerBWon => {
-                if side_to_move == 1 {
-                    0
-                } else {
-                    1
-                }
-            }
+            GameResult::PlayerAWon => 0,
+            GameResult::PlayerBWon => 1,
             GameResult::Draw => 2,
             GameResult::InProgress => unreachable!(),
         };
         packed[14] = packed[14] | result_bits << 30;
-        println!(
-            "[Winner: {:?}, i: {}, side_to_move: {}] -> {} (packed: {:?})",
-            result, i, side_to_move, result_bits, packed
-        );
     }
 
-    file.lock()
-        .unwrap()
-        .write(
-            &packed_positions
-                .into_iter()
-                .flat_map(|p| serialize(p))
-                .collect::<Vec<u8>>(),
-        )
-        .unwrap();
-
-    i
+    packed_positions
 }
 
-//TODO: make sure side to move is always in the top left
-//If the player is player b, we need to flip the board
+//Note: nevermind do not flip the board!!!!! (store stm in the last row)
 fn pack(board: &BoardState, n_wins: usize, n_plays: usize) -> [u32; 15] {
     let mut packed: [u32; 15] = [0; 15];
 
     #[allow(clippy::needless_range_loop)]
     for i in 0..14 {
-        let actual_i = match board.player {
-            Player::White => i,
-            Player::Black => 13 - i,
-        };
-
-        let player_a_data = match board.player {
-            Player::White => board.player_a_bit_board[actual_i],
-            Player::Black => reverse_bits(board.player_b_bit_board[actual_i]),
-        };
-
-        let player_b_data = match board.player {
-            Player::White => board.player_b_bit_board[actual_i],
-            Player::Black => reverse_bits(board.player_a_bit_board[actual_i]),
-        };
+        let player_a_data = board.player_a_bit_board[i];
+        let player_b_data = board.player_b_bit_board[i];
 
         packed[i] = player_a_data as u32 | (player_b_data as u32) << 16;
     }
-    // n_wins, n_plays each take 15 bits
+    // n_wins, n_plays each take 14 bits (so max of 2^14 =)
     // use the top two bits to store result (00 = win, 01 = loss, 10 = tie)
     packed[14] = n_plays as u32 | (n_wins as u32) << 15;
     packed
@@ -147,9 +144,4 @@ fn pack(board: &BoardState, n_wins: usize, n_plays: usize) -> [u32; 15] {
 
 fn serialize(packed: [u32; 15]) -> Vec<u8> {
     packed.into_iter().flat_map(|p| p.to_le_bytes()).collect()
-}
-
-// reverses the 14 least significant bits of a u16
-fn reverse_bits(bits: u16) -> u16 {
-    bits.reverse_bits() >> 2
 }
