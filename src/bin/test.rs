9a16b1625f0245d83446ff61320c49c018fe2cbb
Bulmenisaurus <63682890+Bulmenisaurus@users.noreply.github.com>
2025-08-28 23:45:06 -0700

    update

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,32 +1,39 @@
-use blok_rs::board::{BoardState, GameResult, StartPosition};
-use blok_rs::mcts::MonteCarlo;
+use blok_rs::board::{BoardState, Player, StartPosition};
+
 use blok_rs::movegen::generate_moves;
-use blok_rs::nn::{Accumulator, Network};
-use rand::rng;
-use rand::seq::IndexedRandom;
 
 fn main() {
     // Create a new board in the default start position
     let mut board = BoardState::new(StartPosition::Corner);
-    let mut rng = rand::rng();
 
-    // Play 6 random moves
-    for _ in 0..6 {
-        let moves = generate_moves(&board);
-        if moves.is_empty() {
-            break;
-        }
-        let random_move = moves.choose(&mut rng).unwrap();
-        board.do_move(*random_move);
-    }
+    board.do_move(4096);
+    board.do_move(85465);
 
-    // Now, 10 times, run MCTS and print the best move
-    for i in 0..10 {
-        let mut mcts = MonteCarlo::new();
-        mcts.run_search(&board, "eval");
-        let best_move = mcts.best_play().unwrap();
-        println!("Best move {}: {}", i + 1, best_move);
-        // Optionally, play the move to see the sequence
-        // board.do_move(best_move);
+    let moves = generate_moves(&board);
+
+    let mut moves_with_evals: Vec<(u32, i32)> = moves
+        .iter()
+        .map(|m| {
+            let mut new_board = board.clone();
+            new_board.do_move(*m);
+            let eval = if new_board.player == Player::White {
+                blok_rs::nn::NNUE.evaluate(
+                    &new_board.player_a_accumulator,
+                    &new_board.player_b_accumulator,
+                )
+            } else {
+                blok_rs::nn::NNUE.evaluate(
+                    &new_board.player_b_accumulator,
+                    &new_board.player_a_accumulator,
+                )
+            };
+            (*m, eval)
+        })
+        .collect();
+
+    moves_with_evals.sort_by_key(|(_, eval)| *eval);
+
+    for (m, eval) in moves_with_evals {
+        println!("Move: {}, Eval: {}", m, eval);
     }
 }
